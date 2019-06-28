71c5331dfc774c8046b883fa73166803ef84b31f
Jose Elera <jelera@gmail.com>
2019-06-28 10:53:15 -0500

    Add routes, cors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :ingredients, only: [:index, :show]
+      resources :categories, only: [:index, :show]
+    end
+    namespace :v2 do
+      resources :ingredients, only: [:index, :show]
+      resources :categories, only: [:index, :show]
+    end
+  end
 end
